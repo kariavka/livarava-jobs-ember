ba790ceb7807f74122152987722bd602b4ec883a
kariavka <artem@kariavka.com>
2019-06-04 12:01:38 +0300

    [+] Added Job Model

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,23 +1,25 @@
 import Route from '@ember/routing/route';
 import {inject as service} from '@ember/service';
 import {get} from '@ember/object';
+import RSVP from 'rsvp';
 import AnonymousRouteMixin from 'livarava-jobs-ember/mixins/anonymous-route-mixin';
 
 export default Route.extend(AnonymousRouteMixin, {
   // Services
-  me: service(),
+  session: service(),
+  store: service(),
 
-  // Actions
-  actions: {
+  model(params) {
+    const isAuthenticated = get(this, 'session.isAuthenticated');
 
-    didTransition() {
-      const signed = get(this, 'me.signed');
-      if (signed) {
-        this.transitionTo('index');
-      } else {
-        this.transitionTo('login');
-      }
+    if (!isAuthenticated) {
+      this.transitionTo('login');
     }
 
+    const store = get(this, 'store');
+
+    return RSVP.hash({
+      items: store.query('job', params)
+    });
   },
 });
